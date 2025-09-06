50e74bf15831f23481d3cc692ec8ecad4a5bb13f
Cayque Santos <cayque_santos16@hotmail.com>
2025-09-06 12:34:59 -0300

    feature: adiciona o component ListBooks.

---
+++ src/components/ListBooks.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+import { defineComponent, type PropType } from "vue";
+import type IBook from "../interfaces/IBook";
+
+export default defineComponent({
+  name: "ListBooks",
+  props: {
+    books: {
+      type: Object as PropType<IBook[]>,
+      required: true,
+    },
+  },
+});
+</script>
+
+<template>
+  <ol reversed class="p-8 list-decimal pl-6 space-y-1">
+    <a v-for="book in books.slice().reverse()" :href="book.id">
+      <li>{{ book.title }}</li>
+    </a>
+  </ol>
+</template>
