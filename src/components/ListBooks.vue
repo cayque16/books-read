08cae6af62e6fa5dc6afd038008981065deb8d11
Cayque Santos <cayque_santos16@hotmail.com>
2025-09-07 17:46:24 -0300

    feature: adiciona o clique na lista de livros.

--- src/components/ListBooks.vue
+++ src/components/ListBooks.vue
@@ -1,6 +1,7 @@
 <script lang="ts">
 import { defineComponent, type PropType } from "vue";
 import type IBook from "../interfaces/IBook";
+import { SET_CURRENT_BOOK, useStore } from "../store";
 
 export default defineComponent({
   name: "ListBooks",
@@ -10,11 +11,29 @@ export default defineComponent({
       required: true,
     },
   },
+  setup() {
+    const store = useStore();
+
+    return {
+      store,
+    };
+  },
+  methods: {
+    handleClickedBook(book: IBook) {
+      this.store.commit(SET_CURRENT_BOOK, book);
+    },
+  },
 });
 </script>
 
 <template>
   <ol reversed class="p-8 list-decimal pl-6 space-y-1">
-    <li v-for="book in books.slice().reverse()">{{ book.title }}</li>
+    <li
+      class="cursor-pointer hover:bg-slate-100 active:bg-slate-300 p-1 rounded"
+      v-for="book in books.slice().reverse()"
+      @click="handleClickedBook(book)"
+    >
+      {{ book.title }}
+    </li>
   </ol>
 </template>
